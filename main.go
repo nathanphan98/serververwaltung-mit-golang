d82e81ff9f5ab0a3e373c807d7e26c9e8924463a
p_nhattan <p_nhattan@thk-hd.vn>
2026-09-14 10:02:32 +0700

    refactor code add interface

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	"nathanphan.com/serververwaltung-mit-golang/models"
+	"nathanphan.com/serververwaltung-mit-golang/monitors"
 	"nathanphan.com/serververwaltung-mit-golang/processor"
 )
 
@@ -16,10 +17,17 @@ func main() {
 
 	cn := make(chan models.SystemStat)
 
+	monitors := []models.IMonitor{ // 1 slice chứa 2 struct kiểu interface Monitor
+		&monitors.CPUMonitor{}, // các receiver của struct này yêu cầu * nên giá trị của kiểu interface models.Monitor phải là kiểu con trỏ
+		&monitors.MemoryMonitor{},
+	}
+
 	var wg sync.WaitGroup
-	wg.Add(1)
-	go processor.RunMonitor(ctx, &wg, cn)
-	// dùng goroutine để tránh main chỉ để dùng làm 1 việc
+	for _, m := range monitors {
+		wg.Add(1)
+		go processor.RunMonitor(ctx, &wg, cn, m)
+		// dùng goroutine để tránh main chỉ để dùng làm 1 việc
+	}
 
 	go func() {
 		wg.Wait() // khi nào Wait mở block thì mới chạy xuống close()
--- models/models.go
+++ models/models.go
@@ -1,6 +1,13 @@
 package models
 
-type SystemStat struct{
-	Name string
+import "context"
+
+type SystemStat struct {
+	Name  string
 	Value string
-} 
+}
+
+type IMonitor interface {
+	GetName() string
+	Check(ctx context.Context) string
+}
